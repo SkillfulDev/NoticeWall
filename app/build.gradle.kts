8cfee8ba5e72651cd66023f7983590aad1888fc5
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-08-06 17:37:48 +0300

    added registration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.storage)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
